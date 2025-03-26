34ae334eae8a25b2d3f3cf5955704c52262d6b88
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-03-26 18:02:56 +0800

    update

--- api/suggestions.js
+++ api/suggestions.js
@@ -2,7 +2,45 @@
 import axios from 'axios';
 import jsonpAdapter from 'axios-jsonp';
 
+// import express from 'express';
+// import Cors from 'cors';
+// import fetch from 'node-fetch';  // Use node-fetch for API calls
+/*
+const cors = Cors({
+    methods: ['GET', 'HEAD'],  // Allow only GET and HEAD requests
+    allowedHeaders: ['Content-Type'], // Allow specific headers
+    origin: 'https://media-sphere.vercel.app', // Allow only your frontend app's origin
+});
+
+const app = express();
+
+// Enable CORS for a specific origin (e.g., 'https://media-sphere.vercel.app')
+app.use(cors({
+    origin: 'https://media-sphere.vercel.app'
+}));*/
+
 export default async function handler(req, res) {
+// app.get('/api/suggestions', async (req, res) => {
+// const handler = async (req, res) => {
+    /*
+    await new Promise((resolve, reject) => cors(req, res, (result) => {
+        if (result instanceof Error) {
+        reject(result);
+        } else {
+        resolve();
+        }
+    }));*/
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'https://media-sphere.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const { term } = req.query;
 
   if (!term) {
@@ -71,42 +109,4 @@ export default async function handler(req, res) {
   }
 }
 
-/*
-
-import axios from 'axios';
-import jsonpAdapter from 'axios-jsonp';
-// import jsonp from 'jsonp';  // Import the jsonp package
-// import fetchJsonp from 'fetch-jsonp';
-
-export default async function handler(req, res) {
-  const { term } = req.query;
-
-  if (!term) {
-    return res.status(400).json({ error: 'Search term is required' });
-  }
-
-  const GOOGLE_AC_URL = 'https://clients1.google.com/complete/search';
-  
-  try {
-    // Make the request to the Google Suggest API using axios with jsonpAdapter
-    const response = await axios({
-      url: GOOGLE_AC_URL,
-      adapter: jsonpAdapter,
-      params: {
-        client: 'youtube',  // YouTube-specific client
-        hl: 'en',           // Language parameter
-        ds: 'yt',           // Data source parameter (YouTube)
-        q: term,            // Search term
-      },
-    });
-
-    // Respond with the suggestions (second array in the response)
-    const suggestions = response.data[1].map((item) => item[0]);
-    return res.status(200).json(suggestions);
-  } catch (error) {
-    console.error('Error fetching suggestions:', error);
-    return res.status(500).json({ error: 'Error fetching suggestions' });
-  }
-}
-
-*/
+// export default handler;
