334544a26a5f1c13d56cec42cc64dd2b325f7987
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-04-22 05:45:57 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,31 +1,16 @@
 
 # get trending search keywords of the day
 
-from flask import Flask, jsonify
-from pytrends.request import TrendReq
-
-app = Flask(__name__)
-
-@app.route("/api", methods=["GET"])
-def youtube_trending():
-    pytrends = TrendReq()
-    pytrends.build_payload(kw_list=[], cat=0, timeframe='now 1-d', geo='US', gprop='youtube')
-    trending = pytrends.trending_searches(pn='united_states')
-    return jsonify(trending.head(10).to_dict()[0])  # sends a JSON list of top 10 trends
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-'''
 from pytrends.request import TrendReq
 
 def handler(request):
     pytrends = TrendReq()
     pytrends.build_payload(kw_list=[], cat=0, timeframe='now 1-d', geo='US', gprop='youtube')
+
     trending = pytrends.trending_searches(pn='united_states')
+
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": trending.head(10).to_json()
+        "body": {"trending": trending.tolist()},
+        "headers": {"Content-Type": "application/json"}
     }
-'''
