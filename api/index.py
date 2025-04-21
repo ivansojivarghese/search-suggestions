7cb74417ac994d205a01f38d4eecaff233235be7
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-04-22 05:19:55 +0800

    update

--- api/index.py
+++ api/index.py
@@ -1,6 +1,20 @@
 
 # get trending search keywords of the day
 
+from flask import Flask, jsonify
+from pytrends.request import TrendReq
+
+app = Flask(__name__)
+
+@app.route("/")
+def youtube_trending():
+    pytrends = TrendReq()
+    pytrends.build_payload(kw_list=[], cat=0, timeframe='now 1-d', geo='US', gprop='youtube')
+    trending = pytrends.trending_searches(pn='united_states')
+    return jsonify(trending.head(10).to_dict()[0])  # sends a JSON list of top 10 trends
+
+
+'''
 from pytrends.request import TrendReq
 
 def handler(request):
@@ -12,3 +26,4 @@ def handler(request):
         "headers": {"Content-Type": "application/json"},
         "body": trending.head(10).to_json()
     }
+''''''
